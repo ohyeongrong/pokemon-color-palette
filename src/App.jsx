959002bec466473ee1bbd2e803a6d87865fdf142
ohyeongrong <dhdudfhd920@gmail.com>
2025-10-05 17:11:39 +0900

    feature/pokemon-type-color

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import { getPokemonDetails, getPokemonAllTypes } from './api/pokemonApi.js'
 import usePokemonStore from './stores/usePokemonStore.js'
+import { typeColors } from './constants/typeColors.js'
 
 function App() {
 
@@ -213,119 +214,125 @@ function App() {
       </section>
       <section>
         <h2 className='sr-only'>포켓몬 목록</h2>
-        <div className='grid grid-cols-6 gap-x-3 gap-y-10'>
-          { 
-            pokemonList.map(pokemon => (
-              <article key={pokemon.id}>
-                {/* 반복문 써야함 */}
-                <div className='w-58 h-82'>
-                  <div className='w-full h-full relative'>
-                    {/* 포켓몬 card 앞면*/}
-                    <div
-                      className='absolute
-                      flex flex-col justify-between 
-                      border-1 border-[var(--navy-color)] rounded-2xl 
-                      p-6 w-full h-full backface-hidden'>
-                      {/* 포켓몬 이름 */}
-                      <div className='flex justify-between'>
-                        <div>
-                          <h3 className='text-2xl font-semibold'>{pokemon.name}</h3>
-                          <h2 className='text-sm text-[var(--gray-color)]'>{pokemon.englishName}</h2>
-                        </div>
-                        <dl>
-                          <dt className='sr-only'>도감번호</dt>
-                          <dd className='text-[var(--gray-color)] font-semibold'>#{formatID(pokemon.id)}</dd>
-                        </dl>
-                      </div>
-                      {/* 포켓몬 이미지 */}
-                      <div className='flex flex-col justify-center items-center gap-2 relative'>
-                        <img className='absolute object-cover [image-rendering:pixelated]' src={pokemon.imgGifFrontUrl} alt={pokemon.englishName} />
-                        <div className='w-32 h-32 relative'>
-                          <div className='absolute right-0'>
-                            <button type="button" className='flex p-0.5 rounded-full bg-[var(--gray-color)]'>
-                              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1D1D1F"><path d="M440-440H240q-17 0-28.5-11.5T200-480q0-17 11.5-28.5T240-520h200v-200q0-17 11.5-28.5T480-760q17 0 28.5 11.5T520-720v200h200q17 0 28.5 11.5T760-480q0 17-11.5 28.5T720-440H520v200q0 17-11.5 28.5T480-200q-17 0-28.5-11.5T440-240v-200Z"/></svg>                
-                            </button>
+        <div className='flex flex-col items-center gap-18'>
+          <div className='grid grid-cols-6 gap-x-3 gap-y-10'>
+            { 
+              pokemonList.map(pokemon => (
+                <article key={pokemon.id}>
+                  {/* 반복문 써야함 */}
+                  <div className='w-58 h-82'>
+                    <div className='w-full h-full relative'>
+                      {/* 포켓몬 card 앞면*/}
+                      <div
+                        className='absolute
+                        flex flex-col justify-between 
+                        border-1 border-[var(--navy-color)] rounded-2xl 
+                        p-6 w-full h-full backface-hidden'>
+                        {/* 포켓몬 이름 */}
+                        <div className='flex justify-between'>
+                          <div>
+                            <h3 className='text-2xl font-semibold'>{pokemon.name}</h3>
+                            <h2 className='text-sm text-[var(--gray-color)]'>{pokemon.englishName}</h2>
                           </div>
-                          <div className='w-full h-full bg-[var(--gray-color)]/10 rounded-full'></div>
+                          <dl>
+                            <dt className='sr-only'>도감번호</dt>
+                            <dd className='text-[var(--gray-color)] font-semibold'>#{formatID(pokemon.id)}</dd>
+                          </dl>
                         </div>
-                        {/* 컬러칩 */}
-                        <div className='flex gap-1'>
-                          <div className='w-3 h-3 bg-blue-400 rounded-full shadow-[inset_1px_1px_2px_rgba(0,0,0,0.8)]'></div>
-                          <div className='w-3 h-3 bg-blue-400 rounded-full shadow-[inset_1px_1px_2px_rgba(0,0,0,0.8)]'></div>
-                          <div className='w-3 h-3 bg-blue-400 rounded-full shadow-[inset_1px_1px_2px_rgba(0,0,0,0.8)]'></div>
-                        </div>
-                      </div>
-                      {/* 포켓몬 속성, 타입 */}
-                      <div className='flex justify-between'>
-                        {/* 타입 */}
-                        <ul className='flex gap-1'>
-                          {
-                            pokemon.types.map((type, i) =>
-                              <li key={type.enType + i} className='text-xs bg-emerald-500 rounded-full px-2 py-0.5'>{type.koType}</li>
-                            )
-                          }
-                        </ul>
-                        <dl>
-                          <dt className='sr-only'>분류</dt>
-                          <dd className='text-[13px] text-[var(--gray-color)]'>{pokemon.genus}</dd>
-                        </dl>
-                      </div>
-                    </div>
-                    {/* 포켓몬 card 뒷면*/}
-                    <div 
-                      className='absolute
-                      flex flex-col justify-between 
-                      border-1 border-[var(--navy-color)] bg-[var(--navy-color)]/100 rounded-2xl 
-                      p-6 w-full h-full backface-hidden
-                      rotate-y-180'>
-                      {/* 포켓몬 이름 */}
-                      <div className='flex justify-between'>
-                        <div>
-                          <h3 className='text-2xl font-semibold'>{pokemon.name}</h3>
-                          <h2 className='text-sm text-[var(--gray-color)]'>{pokemon.englishName}</h2>
-                        </div>
-                        <dl>
-                          <dt className='sr-only'>도감번호</dt>
-                          <dd className='text-[var(--gray-color)] font-semibold'>#{formatID(pokemon.id)}</dd>
-                        </dl>
-                      </div>
-                      {/* 포켓몬 이미지 */}
-                      <div className='flex flex-col justify-center items-center gap-2'>
-                        <div className='w-32 h-32 relative'>
-                          <div className='absolute right-0'>
-                            <button type="button" className='flex p-0.5 rounded-full bg-[var(--gray-color)]'>
-                              <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1D1D1F"><path d="M440-440H240q-17 0-28.5-11.5T200-480q0-17 11.5-28.5T240-520h200v-200q0-17 11.5-28.5T480-760q17 0 28.5 11.5T520-720v200h200q17 0 28.5 11.5T760-480q0 17-11.5 28.5T720-440H520v200q0 17-11.5 28.5T480-200q-17 0-28.5-11.5T440-240v-200Z"/></svg>                
-                            </button>
+                        {/* 포켓몬 이미지 */}
+                        <div className='flex flex-col justify-center items-center gap-2 relative'>
+                          <img className='absolute object-cover [image-rendering:pixelated]' src={pokemon.imgGifFrontUrl} alt={pokemon.englishName} />
+                          <div className='w-32 h-32 relative'>
+                            <div className='absolute right-0'>
+                              <button type="button" className='flex p-0.5 rounded-full bg-[var(--gray-color)]'>
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1D1D1F"><path d="M440-440H240q-17 0-28.5-11.5T200-480q0-17 11.5-28.5T240-520h200v-200q0-17 11.5-28.5T480-760q17 0 28.5 11.5T520-720v200h200q17 0 28.5 11.5T760-480q0 17-11.5 28.5T720-440H520v200q0 17-11.5 28.5T480-200q-17 0-28.5-11.5T440-240v-200Z"/></svg>                
+                              </button>
+                            </div>
+                            <div className='w-full h-full bg-[var(--gray-color)]/10 rounded-full'></div>
                           </div>
-                          <img className='w-full h-full object-cover scale-45 [image-rendering:pixelated]' src={pokemon.imgGifBackUrl} alt={pokemon.name} />
-                          <div className='absolute inset-0 w-full h-full bg-[var(--gray-color)]/10 rounded-full'></div>
-                        </div>
-                        {/* 컬러칩 */}
-                        <div className='flex gap-1'>
-                          <div className='w-3 h-3 bg-blue-400 rounded-full shadow-[inset_1px_1px_2px_rgba(0,0,0,0.8)]'></div>
-                          <div className='w-3 h-3 bg-blue-400 rounded-full shadow-[inset_1px_1px_2px_rgba(0,0,0,0.8)]'></div>
-                          <div className='w-3 h-3 bg-blue-400 rounded-full shadow-[inset_1px_1px_2px_rgba(0,0,0,0.8)]'></div>
-                        </div>
-                      </div>
-                      {/* 포켓몬 정보 */}
-                      <div className='flex justify-between'>
-                        <dl className='flex gap-4 text-sm'>
+                          {/* 컬러칩 */}
                           <div className='flex gap-1'>
-                            <dt>키</dt>
-                            <dd className='text-[var(--gray-color)]'>{pokemon.height}m</dd>
+                            <div className='w-3 h-3 bg-blue-400 rounded-full shadow-[inset_1px_1px_2px_rgba(0,0,0,0.8)]'></div>
+                            <div className='w-3 h-3 bg-blue-400 rounded-full shadow-[inset_1px_1px_2px_rgba(0,0,0,0.8)]'></div>
+                            <div className='w-3 h-3 bg-blue-400 rounded-full shadow-[inset_1px_1px_2px_rgba(0,0,0,0.8)]'></div>
                           </div>
+                        </div>
+                        {/* 포켓몬 속성, 타입 */}
+                        <div className='flex justify-between'>
+                          {/* 타입 */}
+                          <ul className='flex gap-1'>
+                            {
+                              pokemon.types.map((type, i) => {
+                                const typeBadge = typeColors[type.koType] || 'bg-gray-300';
+                                return (
+                                  <li key={type.enType + i} className={`text-xs ${typeBadge} rounded-full px-2 py-0.5`}>{type.koType}</li>
+                                )
+                              })
+                            }
+                          </ul>
+                          <dl>
+                            <dt className='sr-only'>분류</dt>
+                            <dd className='text-[13px] text-[var(--gray-color)]'>{pokemon.genus}</dd>
+                          </dl>
+                        </div>
+                      </div>
+                      {/* 포켓몬 card 뒷면*/}
+                      <div 
+                        className='absolute
+                        flex flex-col justify-between 
+                        border-1 border-[var(--navy-color)] bg-[var(--navy-color)]/100 rounded-2xl 
+                        p-6 w-full h-full backface-hidden
+                        rotate-y-180'>
+                        {/* 포켓몬 이름 */}
+                        <div className='flex justify-between'>
+                          <div>
+                            <h3 className='text-2xl font-semibold'>{pokemon.name}</h3>
+                            <h2 className='text-sm text-[var(--gray-color)]'>{pokemon.englishName}</h2>
+                          </div>
+                          <dl>
+                            <dt className='sr-only'>도감번호</dt>
+                            <dd className='text-[var(--gray-color)] font-semibold'>#{formatID(pokemon.id)}</dd>
+                          </dl>
+                        </div>
+                        {/* 포켓몬 이미지 */}
+                        <div className='flex flex-col justify-center items-center gap-2'>
+                          <div className='w-32 h-32 relative'>
+                            <div className='absolute right-0'>
+                              <button type="button" className='flex p-0.5 rounded-full bg-[var(--gray-color)]'>
+                                <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#1D1D1F"><path d="M440-440H240q-17 0-28.5-11.5T200-480q0-17 11.5-28.5T240-520h200v-200q0-17 11.5-28.5T480-760q17 0 28.5 11.5T520-720v200h200q17 0 28.5 11.5T760-480q0 17-11.5 28.5T720-440H520v200q0 17-11.5 28.5T480-200q-17 0-28.5-11.5T440-240v-200Z"/></svg>                
+                              </button>
+                            </div>
+                            <img className='w-full h-full object-cover scale-45 [image-rendering:pixelated]' src={pokemon.imgGifBackUrl} alt={pokemon.name} />
+                            <div className='absolute inset-0 w-full h-full bg-[var(--gray-color)]/10 rounded-full'></div>
+                          </div>
+                          {/* 컬러칩 */}
                           <div className='flex gap-1'>
-                            <dt>몸무게</dt>
-                            <dd className='text-[var(--gray-color)]'>{pokemon.weight}kg</dd>
+                            <div className='w-3 h-3 bg-blue-400 rounded-full shadow-[inset_1px_1px_2px_rgba(0,0,0,0.8)]'></div>
+                            <div className='w-3 h-3 bg-blue-400 rounded-full shadow-[inset_1px_1px_2px_rgba(0,0,0,0.8)]'></div>
+                            <div className='w-3 h-3 bg-blue-400 rounded-full shadow-[inset_1px_1px_2px_rgba(0,0,0,0.8)]'></div>
                           </div>
-                        </dl>
+                        </div>
+                        {/* 포켓몬 정보 */}
+                        <div className='flex justify-between'>
+                          <dl className='flex gap-4 text-sm'>
+                            <div className='flex gap-1'>
+                              <dt>키</dt>
+                              <dd className='text-[var(--gray-color)]'>{pokemon.height}m</dd>
+                            </div>
+                            <div className='flex gap-1'>
+                              <dt>몸무게</dt>
+                              <dd className='text-[var(--gray-color)]'>{pokemon.weight}kg</dd>
+                            </div>
+                          </dl>
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </article>
-            ))
-          }
+                </article>
+              ))
+            }
+          </div>
+          <button className='text-[var(--gray-color)] py-4 w-58 border border-[var(--navy-color)] rounded-lg' type="button">더보기</button>
         </div>
       </section>
     </main>
